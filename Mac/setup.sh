b16d32e02d0b8045bc2a0d884ea0f72051aada7c
thekraftyman <thekraftyman@gmail.com>
2021-06-17 14:10:08 -0500

    add conditionals to setup so you can choose what you want to do

--- Mac/setup.sh
+++ Mac/setup.sh
@@ -8,49 +8,108 @@ echo "██║██║ ╚████║███████║   ██║ 
 echo "╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝";
 echo "";
 
+read "osupdate?Would you like to update the OS? (y/n): "
+read "customfolders?Would you like to create custom folders? (y/n): "
+read "homebrewanswer?Would you like to install homebrew? (y/n): "
+read "pipanswer?Would you like to install pip and ansible? (y/n): "
+read "ohmyzshanswer?Would you like to install oh-my-zsh? (y/n): "
+read "configurationanswer?Would you like to install custom configurations? (requires oh-my-zsh and homebrew) (y/n): "
+read "sshkeysetup?Would you like to set up ssh keys? (y/n): "
+read "gpgkeysetup?Would you like to set up gpg keys? (y/n): "
+
 # update
-echo "Updating OS"
-softwareupdate -i -a
+case ${osupdate:0:1} in
+  y|Y )
+    echo "Updating OS"
+    softwareupdate -i -a
+  ;;
+  * )
+    echo "Skipping OS Update"
+  ;;
+esac
 
 # create folders
-clear
-sleep 1
-echo "Creating folders for convenience"
-sleep 1
-mkdir ~/Documents/python_programs
-mkdir ~/Documents/luther_development
-
-# install brew
-echo "Installing homebrew"
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# install pip
-sudo easy_install pip
+case ${customfolders:0:1} in
+  y|Y )
+    clear
+    sleep 1
+    echo "Creating folders for convenience"
+    sleep 1
+    mkdir ~/Documents/python_programs
+    mkdir ~/Documents/luther_development
+  ;;
+  * )
+    echo "Skipping custom folder creation"
+  ;;
+esac
 
 # run the curl script
 # /bin/zsh ./curl_packages.sh
 
-# run the homebrew script
-/bin/zsh ./brew_packages_installer.sh
+# install brew
+case ${homebrewanswer:0:1} in
+  y|Y )
+    echo "Installing homebrew"
+    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # run the homebrew script
+    /bin/zsh ./brew_packages_installer.sh
+  ;;
+  * )
+    echo "Skipping homebrew installation and its packages"
+  ;;
+esac
 
-# iterm2 information
-clear
-echo "You can import iterm 2 configuration from the config file in ../shared/configurations/ "
+# install pip and ansible
+case ${pipanswer:0:1} in
+  y|Y )
+    sudo easy_install pip
 
-# install ansible
-pip install --user ansible
+    pip install --user ansible
+  ;;
+  * )
+    echo "Skipping pip and ansible installation"
+  ;;
+esac
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+case ${ohmyzshanswer:0:1} in
+  y|Y )
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  ;;
+  * )
+    echo "Skipping oh-my-zsh installation"
+  ;;
+esac
 
 # run the configuration script
-/bin/zsh ./configurations.sh
+case ${configurationanswer:0:1} in
+  y|Y )
+    /bin/zsh ./configurations.sh
+  ;;
+  * )
+    echo "Skipping customization"
+  ;;
+esac
 
 # generate ssh keys to add places
-/bin/zsh ./ssh_keys_setup.sh
+case ${sshkeysetup:0:1} in
+  y|Y )
+    /bin/zsh ./ssh_keys_setup.sh
+  ;;
+  * )
+    echo "Skipping ssh key setup"
+  ;;
+esac
 
 # add gpg setup
-/bin/zsh ./gpg_setup.sh
+case ${gpgkeysetup:0:1} in
+  y|Y )
+    /bin/zsh ./gpg_setup.sh
+  ;;
+  * )
+    echo "Skipping ssh key setup"
+  ;;
+esac
 
 # finally, tell the setup user of the next steps
 /bin/zsh ./next_steps.sh
